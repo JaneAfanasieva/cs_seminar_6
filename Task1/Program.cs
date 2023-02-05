5d2f23b6fc9cbc73e2e9954e4e1684c17ba1508a
Evgenia Afanasieva <electrojanet@gmail.com>
2023-02-05 21:03:09 +0300

    Added folder Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+string numbersLine = Console.ReadLine();
+int[] numbers = GetNumbersFromString(numbersLine);
+int count = GetPositiveNumberCount(numbers);
+System.Console.WriteLine($"[{numbersLine}] -> {count}");
+
+int[] GetNumbersFromString(string numbersLine)
+{
+    string[] numberStrings = numbersLine.Split(' ');
+    int[] numbers = new int[numberStrings.Length];
+
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = Convert.ToInt32(numberStrings[i]);
+    }
+
+    return numbers;
+}
+
+int GetPositiveNumberCount(int[] numbers)
+{
+    int count = 0;
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        if(numbers[i] > 0)
+        {
+            count++;
+        }
+    }
+    
+    return count;
+}
